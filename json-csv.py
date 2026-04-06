d33e201b241f81e606dfe5841d0947ce888e66c0
thanakritgonggraikij <gry.spc@gmail.com>
2026-04-06 12:43:41 -0600

    Commit

--- json-csv.py
+++ json-csv.py
@@ -1,67 +1,69 @@
 import json
-import re
+# import re
 import pandas as pd
 
-#load JSON
-with open("fugue_2005_spacy-processed.json", "r", encoding="utf-8-sig") as f: #! CHANGE DIRECTORY!!!
-    data = json.load(f)
-    
-rows = []
-for item in data:
-    #Call items
-    source = item.get("source", "")
-    page = item.get("page", "")
-    text = item.get("text", "")
-    entities = item.get("entities", [])
-    
-    #Make New row for every entity
-    for entity in entities:
-        ent_text = entity.get("text", "")
-        ent_label = entity.get("label", "")
-        
-        def calculate_location_score(ent_text):
-            score = 0.0
+years = [2011, 2016, 2021] #! CHANGE YEAR!!!!
 
-            #! Check for street keywords
-            street_keywords = ["rue", "av", "boul", "boui", "bld", "pl", "st", "street", "avenue", "boulevard", "place", "est", "nord", "sud", "ouest"] #! CHANGE THIS LIST!!!!
-            if any(keyword in ent_text.lower() for keyword in street_keywords):
-                score += 0.30
+for year in years:
+    #load JSON
+    with open(f"fugue_{year}_spacy-processed.json", "r", encoding="utf-8-sig") as f:
+        data = json.load(f)
+        
+    rows = []
+    for item in data:
+        #Call items
+        source = item.get("source", "")
+        page = item.get("page", "")
+        text = item.get("text", "")
+        entities = item.get("entities", [])
+        
+        #Make New row for every entity
+        for entity in entities:
+            ent_text = entity.get("text", "")
+            ent_label = entity.get("label", "")
             
-            #! Check for keywords from keywords.txt (example list)
-            with open("keywords.txt", "r", encoding="utf-8-sig") as f:
-                keywords = [line.strip() for line in f]
-            if any(keyword in ent_text.lower() for keyword in keywords):
-                score += 0.20
-            
-            #! Check for city names (example list)
-            city_names = ["Montréal", "mtl", "Québec", "montreal", "laval", "saint-laurent", "parc"] #! Update List if required!!!!
-            if any(city in ent_text.lower() for city in [name.lower() for name in city_names]):
-                score += 0.20
-            
-            #! Check for postal code pattern XXX XXX
-            if re.match(r"\d{3} \d{3}", ent_text):
-                score += 0.15
-            
-            #! Check length
-            if len(ent_text) > 10:
-                score += 0.15
-            
-            return min(score, 1.0)
+            # def calculate_location_score(ent_text):
+            #     score = 0.0
 
-        ent_loc_score = calculate_location_score(ent_text) #! Location Score
-        
-        
-        rows.append({
-            "source": source,
-            "page": page,
-            "text": text,
-            "entity_text": ent_text,
-            "entity_label": ent_label,
-            "entity_loc_score": ent_loc_score
-        })
-        
-#Create DataFrame + Export as CSV
-df = pd.DataFrame(rows)
-df.to_csv("fugue_2005_spacy-processed.csv", index=False, encoding="utf-8-sig") #! CHANGE DIRECTORY!!!!
+            #     #! Check for street keywords
+            #     street_keywords = ["rue", "av", "boul", "boui", "bld", "pl", "st", "street", "avenue", "boulevard", "place", "est", "nord", "sud", "ouest"] #! CHANGE THIS LIST!!!!
+            #     if any(keyword in ent_text.lower() for keyword in street_keywords):
+            #         score += 0.30
+                
+            #     #! Check for keywords from keywords.txt (example list)
+            #     with open("keywords.txt", "r", encoding="utf-8-sig") as f:
+            #         keywords = [line.strip() for line in f]
+            #     if any(keyword in ent_text.lower() for keyword in keywords):
+            #         score += 0.20
+                
+            #     #! Check for city names (example list)
+            #     city_names = ["Montréal", "mtl", "Québec", "montreal", "laval", "saint-laurent", "parc"] #! Update List if required!!!!
+            #     if any(city in ent_text.lower() for city in [name.lower() for name in city_names]):
+            #         score += 0.20
+                
+            #     #! Check for postal code pattern XXX XXX
+            #     if re.match(r"\d{3} \d{3}", ent_text):
+            #         score += 0.15
+                
+            #     #! Check length
+            #     if len(ent_text) > 10:
+            #         score += 0.15
+                
+            #     return min(score, 1.0)
+            # ent_loc_score = calculate_location_score(ent_text) #! Location Score
+            
+            rows.append({
+                "source": source,
+                "page": page,
+                "text": text,
+                "entity_text": ent_text,
+                "entity_label": ent_label,
+                # "entity_loc_score": ent_loc_score
+            })
+            
+    #Create DataFrame + Export as CSV
+    df = pd.DataFrame(rows)
 
-print(f"CSV created with {len(df)} rows")
+    #! OUTPUT
+    df.to_csv(f"fugue_{year}_spacy-processed.csv", index=False, encoding="utf-8-sig") 
+    print(f"CSV created with {len(df)} rows")
--- spacy-parser.py
+++ spacy-parser.py
@@ -11,7 +11,7 @@ import json
 print("LET'S GO")
 nlp = spc.load("C:\\Users\\13065\\Documents\\GitHub\\queering-geographies\\spacy-training\\output\\model-best") #Use this for our TRAINED MODEL
 
-with open("fugue_2005_for-spacy.json", "r", encoding="utf-8") as f: #! CHANGE DIRECTORY!!!!
+with open("fugue_2021_for-spacy.json", "r", encoding="utf-8") as f: #! CHANGE DIRECTORY!!!!
     blocks = json.load(f)
 
 print("Loaded and Read")
@@ -39,7 +39,7 @@ for block, doc in zip(blocks, docs):
     i += 1
 
 print("Blocks processed, writing to JSON...")
-with open("fugue_2005_spacy-processed.json", "w", encoding="utf-8") as f: #! CHANGE DIRECTORY!!!!
+with open("fugue_2021_spacy-processed.json", "w", encoding="utf-8") as f: #! CHANGE DIRECTORY!!!!
     json.dump(output, f, ensure_ascii=False, indent=2)
 
 print(f"{len(output)} Rows Total")
